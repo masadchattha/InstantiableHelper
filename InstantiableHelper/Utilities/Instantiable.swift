89f4e8e617ffd753606e43d7a413dd7a71bc8aac
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-12-12 22:03:35 +0500

    chore: add Instantiable protocol

---
+++ InstantiableHelper/Utilities/Instantiable.swift
@@ -0,0 +1,29 @@
+//
+//  Instantiable.swift
+//  InstantiableHelper
+//
+//  Make Controllers easy to instantiate by calling just <#instantiate#>
+//
+//
+//  Created by Muhammad Asad Chattha on 12/12/2023.
+//
+
+import UIKit
+
+protocol Instantiable {
+    static var storyboard: UIStoryboard { get }
+}
+
+// MARK: - Providing default implementation
+extension Instantiable {
+
+    static func instantiate() -> Self {
+        return instantiateFromStoryboard()
+    }
+
+    private static func instantiateFromStoryboard<T>() -> T {
+        let identifier = String(describing: self)
+        return storyboard.instantiateViewController(withIdentifier: identifier) as! T
+    }
+}
+
